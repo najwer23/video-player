93de5501cad1b5c5fb19a3929740cf02f971043c
Travis CI <najwer23@gmail.com>
2019-11-03 17:31:27 +0100

    finished range input: Edge, Mozilla, Chrome

--- JS/video.js
+++ JS/video.js
@@ -1,6 +1,9 @@
 window.onload = function () {
 
-  
+    const inputRange = document.querySelector('#videoDanceAtBarcelona .videoBar');
 
-
+    inputRange.addEventListener('input', function () {
+        inputRange.style.setProperty('--val', +inputRange.value)
+    }, false);
+    
 }
