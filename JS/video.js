6a6c92dc189fddd3005addfe1f3428551470b7f7
Travis CI <najwer23@gmail.com>
2020-01-21 01:15:20 +0100

    play-pause button

--- JS/video.js
+++ JS/video.js
@@ -1,9 +1,27 @@
 window.onload = function () {
 
     const inputRange = document.querySelector('#videoDanceAtBarcelona .videoBar');
-
     inputRange.addEventListener('input', function () {
         inputRange.style.setProperty('--val', +inputRange.value)
     }, false);
-    
+
+    const video = document.querySelector('#videoDanceAtBarcelona #video');
+    const playButton = document.querySelector('#videoDanceAtBarcelona #play');
+    const pauseButton = document.querySelector('#videoDanceAtBarcelona #pause');
+   
+    playButton.addEventListener("click", e => {
+        if (video.paused) {
+            video.play();
+            playButton.style.display = "none";
+            pauseButton.style.display = "block";
+        }   
+    });
+
+    pauseButton.addEventListener("click", e => {
+        if (!video.paused) {
+            video.pause();
+            playButton.style.display = "block";
+            pauseButton.style.display = "none";
+        }
+    });
 }
